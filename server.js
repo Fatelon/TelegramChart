8906e52d85eca81951eb24ae52feb5d8479c41b6
spetrov <spetrov@natlex.ru>
2019-03-21 23:25:57 +0300

    first minify

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/tCharts'));
+app.use(express.static(__dirname + '/mini'));
 
 app.listen(process.env.PORT || 8080);
